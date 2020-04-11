c2965279c763f2b7f7a1cf71320126959d06e662
Pikul86 <pikuliurii@gmail.com>
2020-04-11 17:15:31 +0700

    Доделал страницу index-mobile

---
+++ source/js/popup.js
@@ -0,0 +1,15 @@
+var link = document.querySelector(".top__svg");
+var popup = document.querySelector(".menu");
+var close = document.querySelector(".menu__close")
+
+
+
+popup.classList.add("menu-none");
+link.addEventListener("click", function (evt) {
+    evt.preventDefault();
+    popup.classList.remove("menu-none");
+});
+close.addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup.classList.add("menu-none");
+});
