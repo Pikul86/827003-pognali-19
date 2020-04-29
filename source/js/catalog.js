ee2749e6f906bdd0c4039743bd84745cd2bb9332
Pikul86 <pikuliurii@gmail.com>
2020-04-29 21:48:11 +0700

    исправил имена файлов в файле catalog.js

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -3,24 +3,24 @@ var dots = document.querySelector(".dots");
 var close = document.querySelector(".catalog-close-top");
 var closebottom = document.querySelector(".close");
 
-modal.classList.add("modal-none");
-close.classList.add("catalog-close-none");
+modal.classList.add("modal-delete");
+close.classList.add("catalog-close");
 
 dots.addEventListener("click", function (evt) {
   evt.preventDefault();
-  modal.classList.remove("modal-none");
-  dots.classList.add("dots-none");
-  close.classList.remove("catalog-close-none");
+  modal.classList.remove("modal-delete");
+  dots.classList.add("dots-delete");
+  close.classList.remove("catalog-close");
 });
 
 close.addEventListener("click", function (evt) {
   evt.preventDefault();
-  close.classList.add("catalog-close-none");
-  modal.classList.add("modal-none");
-  dots.classList.remove("dots-none");
+  close.classList.add("catalog-close");
+  modal.classList.add("modal-delete");
+  dots.classList.remove("dots-delete");
 });
 closebottom.addEventListener("click", function (evt) {
-  close.classList.add("catalog-close-none");
-  modal.classList.add("modal-none");
-  dots.classList.remove("dots-none");
+  close.classList.add("catalog-close");
+  modal.classList.add("modal-delete");
+  dots.classList.remove("dots-delete");
 });
