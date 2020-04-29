647783830efc310ca5408daedcacb2461802a5c2
Pikul86 <pikuliurii@gmail.com>
2020-04-29 22:33:57 +0700

    Последние изминения на 29.04.

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -2,6 +2,9 @@ var modal = document.querySelector(".modal-filter");
 var dots = document.querySelector(".dots");
 var close = document.querySelector(".catalog-close-top");
 var closebottom = document.querySelector(".close");
+var closebuttondown = document.querySelector(".best-companion__button--down");
+var checkbox = document.querySelector(".best-companion__checkbox");
+var btntransport = document.querySelector(".btn-transport__companion");
 
 modal.classList.add("modal-delete");
 close.classList.add("catalog-close");
@@ -19,8 +22,10 @@ close.addEventListener("click", function (evt) {
   modal.classList.add("modal-delete");
   dots.classList.remove("dots-delete");
 });
+
 closebottom.addEventListener("click", function (evt) {
   close.classList.add("catalog-close");
   modal.classList.add("modal-delete");
   dots.classList.remove("dots-delete");
 });
+
