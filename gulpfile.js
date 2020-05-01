ab23bf0486bf82fb380c66afdb0570bb7b98bb5a
Pikul86 <pikuliurii@gmail.com>
2020-05-01 14:33:00 +0700

    Доделал картинки в Form под ретину

--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task("server", function () {
     notify: false,
     open: true,
     cors: true,
-    ui: false, startPath: "/catalog.html"
+    ui: false, startPath: "/form.html"
   });
 
   gulp.watch("source/less/**/*.less", gulp.series("css"));
