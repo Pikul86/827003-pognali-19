dcd2b92eafb106966aafa549c841d87d3a3ff2d0
Pikul86 <pikuliurii@gmail.com>
2020-05-05 14:35:26 +0700

    Последние изминения

--- source/js/catalog.js
+++ source/js/catalog.js
@@ -1,27 +1,24 @@
 var modal = document.querySelector(".modal-filter");
 var dots = document.querySelector(".dots");
 var close = document.querySelector(".catalog-close-top");
-var closetwo = document.querySelector(".alphabet__close");
+var closetwo = document.querySelectorAll(".alphabet__close");
 var viewall = document.querySelector(".filter-country__text");
 
+function changeCloseState(evt) {
+  evt.preventDefault();
+  close.classList.add("catalog-close");
+  modal.classList.add("modal-delete");
+  dots.classList.remove("dots-delete");
+  viewall.classList.remove("dots-delete");
+};
 
 dots.classList.add("dots-delete");
 viewall.classList.add("dots-delete");
 
-close.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  close.classList.add("catalog-close");
-  modal.classList.add("modal-delete");
-  dots.classList.remove("dots-delete");
-  viewall.classList.remove("dots-delete");
-});
+close.addEventListener("click", changeCloseState);
 
-closetwo.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  close.classList.add("catalog-close");
-  modal.classList.add("modal-delete");
-  dots.classList.remove("dots-delete");
-  viewall.classList.remove("dots-delete");
+Array.prototype.slice.call(closetwo,0).forEach(element => {
+  element.addEventListener("click", changeCloseState);
 });
 
 dots.addEventListener("click", function (evt) {
